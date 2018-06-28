10962414e8a1b4f810e0770a26083afdd306a3fc
student <Kuganeshan.nair@gmail.com>
2018-06-28 18:02:47 +0800

    Added mlab

--- filemgr.js
+++ filemgr.js
@@ -2,7 +2,7 @@ const {MongoClient}= require('mongodb');
 const fs = MongoClient;
 
 const database = 'mongodb://localhost:27017';
-// const database = 'mongodb://KN:iamkugan27@ds245150.mlab.com:45150/weather_app';
+ const database = 'mongodb://places:iamkugan27/@ds141870.mlab.com:41870/placeapp';
 const appname = 'placesapp';
 const collectionname = 'placesappcollection';
 
