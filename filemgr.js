a62d7a7bac8587532b9a2067cc91c086e2df6810
student <Kuganeshan.nair@gmail.com>
2018-07-03 10:30:34 +0800

    fixed heroku mongodb

--- filemgr.js
+++ filemgr.js
@@ -3,7 +3,8 @@ const fs = MongoClient;
 
 //const database = 'mongodb://localhost:27017';
 //const database = 'mongodb://places:iamkugan27/@ds141870.mlab.com:41870/placeapp';
-const database = 'mongodb://places:iamkugan27/@ds141870.mlab.com:41870/placeapp';
+//const database = 'mongodb://places:iamkugan27/@ds141870.mlab.com:41870/placeapp';
+const database = 'mongodb://kugan:kugan123@ds147190.mlab.com:47190/placesapp';
 
 const appname = 'placeapp';
 const collectionname = 'placesappcollection';
