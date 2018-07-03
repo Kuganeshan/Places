b20e7772eae3141b6d94b05f256924fc7c7d4b58
student <Kuganeshan.nair@gmail.com>
2018-07-03 10:46:59 +0800

    replaced filemgr.js

--- filemgr.js
+++ filemgr.js
@@ -1,4 +1,4 @@
-const {MongoClient}= require('mongodb');
+const {MongoClient} = require('mongodb');
 const fs = MongoClient;
 
 //const database = 'mongodb://localhost:27017';
@@ -6,76 +6,78 @@ const fs = MongoClient;
 //const database = 'mongodb://places:iamkugan27/@ds141870.mlab.com:41870/placeapp';
 const database = 'mongodb://kugan:kugan123@ds147190.mlab.com:47190/placesapp';
 
-const appname = 'placeapp';
+const appname = 'placesapp';
 const collectionname = 'placesappcollection';
 
 const saveData = (newdata) => {
   return new Promise((resolve, reject) => {
     MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
       if (err) {
-      reject('Unable to connect to MongoDB');
-    }
-    console.log('Connected to MongoDB');
-    const db = client.db(appname);
+        reject('Unable to connect to MongoDB');
+      }
 
-const length = newdata.length;
-for(var i=0; i<length; i++){
-    db.collection(collectionname).insertOne(newdata[i],(err, result) => {
-       if (err) {
-         reject('Unable to insert');
-       }
-     });
-   }
-  resolve(1);
+      console.log('Connected to MongoDB');
+      const db = client.db(appname);
 
-    client.close();
+      const length = newdata.length;
+      for(var i=0; i<length; i++) {
+        db.collection(collectionname).insertOne(newdata[i], (err, result) => {
+          if (err) {
+            reject('Unable to insert');
+          }
+
+        });
+      }
+
+      resolve(1);
+
+      client.close();
     });
   });
 };
-
 
 const getAllData = () => {
-  return new Promise((resolve, reject) =>{
+  return new Promise((resolve, reject) => {
     MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
       if (err) {
-      reject('Unable to connect to MongoDB');
-    }
-    console.log('Connected to MongoDB');
-    const db = client.db(appname);
+        reject('Unable to connect to MongoDB');
+      }
 
-    db.collection(collectionname).find().toArray().then((docs) => {
-    resolve(docs);
-    }, (err) => {
-      reject('Unable to fetch docs');
+      console.log('Connected to MongoDB');
+      const db = client.db(appname);
+
+      db.collection(collectionname).find().toArray().then( (docs) => {
+        resolve(docs);
+      }, (err) => {
+        reject('Unable to fetch docs');
+      });
+
+      client.close();
     });
-
-    client.close();
-    });
-
   });
 };
+
 const deleteAll = () => {
-  return new Promise((resolve, reject) =>{
+  return new Promise((resolve, reject) => {
     MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
       if (err) {
-      reject('Unableto connect to MongoDB');
-    }
-    console.log('Connected to MongoDB');
-    const db = client.db(appname);
+        reject('Unable to connect to MongoDB');
+      }
 
+      console.log('Connected to MongoDB');
+      const db = client.db(appname);
 
+      db.collection(collectionname).remove({}).then( (result) => {
+        resolve(result);
+      }, (err) => {
+        reject('Unable to delete');
+      });
 
-    db.collection(collectionname).remove({}).then((result) => {
-    resolve(result);
-    }, (err) => {
-      reject('Unable to delete');
+      client.close();
     });
-
-    client.close();
-    });
-
   });
 };
+
 
 
 
